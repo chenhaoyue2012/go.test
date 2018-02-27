1f5ba0d3568e48ca003fa316c5709b0a6aa134b3
陈志坤 <chenzhikun@17shanyuan.com>
2018-02-27 12:17:10 +0800

    模板测试

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"net/http"
-
+	"html/template"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"io"
 )
 
+
+type	Template	struct{
+	templates	*template.Template
+}
+
+func(t	*Template)	Render(w	io.Writer,	name	string,	data	interface {},	c	echo.Context)	error{
+	return	t.templates.ExecuteTemplate(w,	name,	data)
+}
+
+func	Hello(c	echo.Context)	error{
+	return	c.Render(http.StatusOK,"hello","zhangsan")
+}
+
+
 func main() {
+	t :=&Template{
+		templates:	template.Must(template.ParseGlob("public/views/*.html")),
+	}
+
 	// Echo instance
 	e := echo.New()
+	e.Renderer	=	t
 
 	// Middleware
 	e.Use(middleware.Logger())
@@ -17,8 +37,10 @@ func main() {
 
 	// Route => handler
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!\n")
+		return c.String(http.StatusOK, "Hello, moon!\n")
 	})
+
+	e.GET("/hello",	Hello)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
